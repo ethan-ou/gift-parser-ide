de68f10fe68212ab5e47e2bed2bea66b47fb0acb
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2020-06-09 12:47:35 +1000

    Create script for updating gift parser

---
+++ scripts/updateParser.js
@@ -0,0 +1,52 @@
+var https = require("https");
+var fs = require("fs-extra");
+var path = require("path");
+var peg = require("pegjs");
+var tspeg = require("ts-pegjs");
+
+var grammar =
+  "https://raw.githubusercontent.com/fuhrmanator/GIFT-grammar-PEG.js/master/GIFT.pegjs";
+
+var downloadLocation = path.join(__dirname, "/GIFT.pegjs");
+var parserLocation = path.join(__dirname, "../src/parser/parser.ts");
+
+var dirCheck = function (download, parser) {
+  fs.ensureFileSync(download);
+  fs.ensureFileSync(parser);
+};
+
+var download = function (url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = https
+    .get(url, function (response) {
+      response.pipe(file);
+      file.on("finish", function () {
+        file.close(cb); // close() is async, call cb after close completes.
+      });
+    })
+    .on("error", function (err) {
+      // Handle errors
+      fs.unlink(dest); // Delete the file async. (But we don't check the result)
+      if (cb) cb(err.message);
+    });
+};
+
+var createParser = function (file, dest) {
+  try {
+    var grammar = peg.generate(fs.readFileSync(file, "utf8"), {
+      output: "source",
+      format: "commonjs",
+      plugins: [tspeg],
+    });
+
+    fs.writeFileSync(dest, grammar);
+    console.log("Grammar Created.");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+dirCheck(downloadLocation, parserLocation);
+download(grammar, downloadLocation, () => {
+  createParser(downloadLocation, parserLocation);
+});
