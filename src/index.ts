a68bb4cc07a1feed0c8e9da9fee67a43dd5d8e26
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2020-06-10 01:09:24 +1000

    Remove previous

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,4 @@
-import Parser from "./gift";
-import chevrotain from "chevrotain";
+import split from "./split";
+import mocks from "./__tests__/mocks";
 
-const result = Parser.parse("Hello");
-
-console.log(result);
-console.log(result.parseErrors);
-
-// const parserInstance = Parser.parserInstance;
-// const serializedGrammar = parserInstance.getSerializedGastProductions();
-
-// create the HTML Text
-// const htmlText = chevrotain.createSyntaxDiagramsCode(serializedGrammar);
-// console.log(htmlText);
-
-// export function parseGIFT(text: string) {
-//     const lexResult = Parser.tokenize(text);
-//     // setting a new input will RESET the parser instance's state.
-//     Parser.input = lexResult.tokens;
-//     // any top level rule may be used as an entry point
-//     const cst = Parser.json();
-  
-//     // this would be a TypeScript compilation error because our parser now has a clear API.
-//     // let value = parser.json_OopsTypo()
-  
-//     return {
-//       // This is a pure grammar, the value will be undefined until we add embedded actions
-//       // or enable automatic CST creation.
-//       cst: cst,
-//       lexErrors: lexResult.errors,
-//       parseErrors: Parser.errors
-//     }
-//   }
+console.log(split(mocks[2].text));
