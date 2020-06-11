6ec5c94440fd12b7d21b4abfad1723070cc39d7c
ethan-ou <ethantim@gmail.com>
2020-06-12 01:10:32 +1000

    Move types

--- src/types.d.ts
+++ src/types.d.ts
@@ -25,3 +25,22 @@ interface IParse {
 }
 
 type IResult = IParse | IError;
+
+interface Parse {
+  token: string;
+  line: number;
+  char: number;
+}
+
+type TextToken =
+  | "SCOPE_OPEN"
+  | "SCOPE_CLOSED"
+  | "EMPTY_LINE"
+  | "SCOPE"
+  | undefined;
+
+interface TextSplit {
+  start: number;
+  end: number;
+  text: string;
+}
