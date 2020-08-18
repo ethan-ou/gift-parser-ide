b55ed21a0870b1c2dc13861d95612bf187c7b450
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2020-08-18 22:42:32 +1000

    Move to using .ts over ambient declaration for build

--- src/types.ts
+++ src/types.ts
@@ -1,46 +1,46 @@
 import { Expectation, IFileRange, SyntaxError } from "./parser/PEGParser";
 
-interface GIFTSyntaxError extends SyntaxError {}
+export interface GIFTSyntaxError extends SyntaxError {}
 
-type ParseType = "result" | "error";
+export type ParseType = "result" | "error";
 
-interface ParseReturn {
+export interface ParseReturn {
   type: Extract<"result", ParseType>;
   result: any;
 }
 
-interface ErrorReturn {
+export interface ErrorReturn {
   type: Extract<"error", ParseType>;
   result: GIFTSyntaxError;
 }
 
-interface ErrorReturnArr {
+export interface ErrorReturnArr {
   type: Extract<"error", ParseType>;
   result: GIFTSyntaxError[];
 }
 
-interface ErrorResult extends ErrorReturn {
+export interface ErrorResult extends ErrorReturn {
   start: number;
   end: number;
   text: string;
 }
 
-interface ErrorResultArr extends ErrorReturnArr {
+export interface ErrorResultArr extends ErrorReturnArr {
   start: number;
   end: number;
   text: string;
 }
 
-interface ParseResult extends ParseReturn {
+export interface ParseResult extends ParseReturn {
   start: number;
   end: number;
   text: string;
 }
 
-interface TextSplit {
+export interface TextSplit {
   start: number;
   end: number;
   text: string;
 }
 
-type GIFTResult = ParseResult | ErrorResultArr;
+export type GIFTResult = ParseResult | ErrorResultArr;
